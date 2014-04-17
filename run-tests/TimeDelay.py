e63d79d6a1c1e303e61aedd9fed68585c47c179b
Asif Sardar <engr.asif.sardar@gmail.com>
2014-04-17 16:33:46 +0300

    Changed the shell script files to python scripts ...

---
+++ run-tests/TimeDelay.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from subprocess import call
+
+TESTNAME='TimeDelay'
+TESTPATH='/home/asif/Ell-i/Ell-i-Pybot/ELL-i-PyBot-Tests/test-scripts/'
+
+call(
+	'pybot'+
+	' --variable TESTPATH:'+TESTPATH+ 
+	' --log ../test-results/'+TESTNAME+'/log.html'+ 
+	' --report ../test-results/'+TESTNAME+'/report.html'+ 
+	' --output ../test-results/'+TESTNAME+'/output.xml'+ 
+	' ../test-suites/ELL-i-PyBot-Tests.wiki/'+TESTNAME+'.rest',
+	shell=True
+	)
