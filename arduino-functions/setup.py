06bc4ba43f74c7131f1f0cbee30be627f137f1be
Asif Sardar <engr.asif.sardar@gmail.com>
2014-04-24 17:13:11 +0300

    Adding the pin mode module for c-python integration

--- arduino-functions/setup.py
+++ arduino-functions/setup.py
@@ -1,3 +1,4 @@
 from distutils.core import setup, Extension
-setup(name='digitalRead', version='1.0', ext_modules=[Extension('digitalRead', ['DigitalRead/digitalRead.c'])])
-setup(name='digitalWrite', version='1.0', ext_modules=[Extension('digitalWrite', ['DigitalWrite/digitalWrite.c'])])
+setup(name='digitalRead', version='1.0', ext_modules=[Extension('digitalRead', ['Digital-I-O/digitalRead.c'])])
+setup(name='digitalWrite', version='1.0', ext_modules=[Extension('digitalWrite', ['Digital-I-O/digitalWrite.c'])])
+setup(name='pinMode', version='1.0', ext_modules=[Extension('pinMode', ['Digital-I-O/pinMode.c'])])
