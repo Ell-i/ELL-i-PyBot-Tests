c4ae5d0f30484f8dd2b7278fc16ab2bc242fad8a
Asif Sardar <engr.asif.sardar@gmail.com>
2014-05-26 17:43:01 +0300

    Wrote test suite for serial peripheral interface. Seperated the transmit and recieve test suites for communication API e.g. serial communication.

---
+++ run-tests/SerialPeripheralInterface.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import os
+from subprocess import call
+
+
+TESTNAME='SerialPeripheralInterface'
+TESTPATH=os.environ['HOME']+'/Ell-i/Ell-i-Pybot/ELL-i-PyBot-Tests/test-scripts/'
+
+print TESTPATH
+
+call(
+	'pybot'+
+	' --variable TESTPATH:'+TESTPATH+ 
+	' --outputdir ../test-results/'+TESTNAME+
+	' ../test-suites/ELL-i-PyBot-Tests.wiki/'+TESTNAME+'.rest',
+	shell=True
+	)
---
+++ test-scripts/SerialPeripheralInterface.py
@@ -0,0 +1,27 @@
+"""
+TEST CASES START
+"""
+
+#Import an SPI c-python library here to call the SPI functions ...
+
+def begin_spi():
+	"""Begin the SPI port"""
+
+def end_spi():
+	"""End the SPI port"""
+
+def bit_order(bitOrder):
+	"""Sets the bit order to LSB or MSB"""
+
+def clock_divider(clockDivider):
+	"""Sets the clock divider to 2, 4, 8, 16, 32, 64 or 128"""
+
+def data_mode(dataMode):
+	""""Sets the data mode to 0, 1, 2 or 3"""
+
+def transfer_value(value):
+	"""Transfer the value from SPI port using the MOSI pin"""
+
+"""
+TEST CASES END
+"""
