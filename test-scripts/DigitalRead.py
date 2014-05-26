d55365ce1e343f890ae1e114497db1205cb2ab52
Asif Sardar <engr.asif.sardar@gmail.com>
2014-05-26 19:18:34 +0300

    Instead of implementing, compiling and installing modules for C integration, we may use the python ctypes module to load the shared library (e.g. emulator) and access the methods (e.g. pinMode(), digitalRead() or digitalWrite()) directly from python scripts or test libraries. This would ease the C-Python code integration, eliminating the nead for unnecessary coding for C-Python integration.

--- test-scripts/DigitalRead.py
+++ test-scripts/DigitalRead.py
@@ -2,24 +2,38 @@
 TEST CASES START
 """
 
-import digitalRead
+from ctypes import *
+
+#Load the emulator shared library. Call the c-functions directly from the python script using ctypes modules.
+emulator = CDLL("nameofsharedlibrary.so")
+
+#import digitalRead
 
 #print digitalRead.readHigh()
 
 def set_pin_mode(pin):
     """Set the pin mode to input"""
+    emulator.pinMode(pin, INPUT);
+
 
 def read_high(pin):
     """Read 'HIGH' from pin"""
     #print 'Here it uses test library to interact with Ell-i runtime C code for reading input value from pin.'
     #"""Also returns value for pin"""
     #return digitalRead.readHigh(pin)
+    high = c_bool();
+    high = emulator.digitalRead(pin);
+    return high;
+
 
 def read_low(pin):
     """Read 'LOW' from pin"""
     #print 'Here it uses test library to interact with Ell-i runtime C code for reading input value from pin.'
     #"""Also returns value for pin"""
     #return digitalRead.readLow(pin)
+    low = c_bool();
+    low = emulator.digitalRead(pin);
+    return low; 
 
 """
 TEST CASES END
