eb4604a91f141f9d281f22ad0916e8a507be5dad
Asif Sardar <engr.asif.sardar@gmail.com>
2014-06-23 15:53:02 +0300

    ...

--- test-scripts/DigitalWrite.py
+++ test-scripts/DigitalWrite.py
@@ -12,17 +12,12 @@ emulator = CDLL("../libemulator.so")
 #import digitalWrite
 
 def set_pin_mode(port, pin):
-    """Set the pin mode to output"""
     emulator.pinMode(GPIO[port]['PIN'+str(pin)], PinMode['OUTPUT']);
 
 def write_high(port, pin):
-    """Write 'HIGH' to pin"""
-    #print 'Here it uses test library to interact with Ell-i runtime C code for writing value to pin.'
     emulator.digitalWrite(GPIO[port]['PIN'+str(pin)], PinValue['HIGH']);
 
 def write_low(port, pin):
-    """Write 'LOW' to pin"""
-    #print 'Here it uses test library to interact with Ell-i runtime C code for writing value to pin.'
     emulator.digitalWrite(GPIO[port]['PIN'+str(pin)], PinValue['LOW']);
 
 """
