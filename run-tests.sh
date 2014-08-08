a4553165cbdfef276a69e644a58abb97d0732c90
Lari Lehtomäki <lari@lehtomaki.fi>
2014-08-08 15:39:29 +0300

    Renamed the the test-suites folder to testcases, mainly to not get in trouble with previous nested test-suites git repository.

--- run-tests.sh
+++ run-tests.sh
@@ -13,13 +13,13 @@ export PATH_TO_TESTS=`dirname $_`
 export PYTHONPATH=$PATH_TO_TESTS/test-scripts:$PYTHONPATH
 
 run_test() {
-    pybot  --outputdir $PATH_TO_TESTS/test-results/$1 $PATH_TO_TESTS/test-suites/$1.rest
+    pybot  --outputdir $PATH_TO_TESTS/test-results/$1 $PATH_TO_TESTS/testcases/$1.rest
 }
 
 
 case "$1" in
   list)
-        ls -1 test-suites/|grep ".rest"|awk -F '.' '{print $1}'
+        ls -1 testcases/|grep ".rest"|awk -F '.' '{print $1}'
         ;;
   help)
         echo "Usage: ./run-tests.sh command [parameter]"
@@ -34,7 +34,7 @@ case "$1" in
 	echo "          help           This help document"
         ;;
   run-all)
-	TESTS=`ls -1 test-suites/|grep ".rest"|awk -F '.' '{print $1}'`
+	TESTS=`ls -1 testcases/|grep ".rest"|awk -F '.' '{print $1}'`
 	for TEST in $TESTS; do
 	    run_test $TEST
 	done
