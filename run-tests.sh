171069492419394c502c37e4c8304e2f407f7ff5
Asif Sardar <engr.asif.sardar@gmail.com>
2014-08-20 11:47:03 +0300

    Made enhancements to the run-tests shell script ...

--- run-tests.sh
+++ run-tests.sh
@@ -1,53 +1,106 @@
 #!/bin/sh
 
-## Path to ELL-i Runtime
-export ELLIRUNTIME="/home/asif/Ell-i-Working-Directory/Ell-i-Software-Development/Runtime"
 #export ELLIRUNTIME="../Runtime"
 
+#############################################################################################
+## Setting path variables by users.
+#############################################################################################
+# Path to ELL-i Runtime
+export ELLIRUNTIME="/home/asif/Ell-i-Working-Directory/Ell-i-Software-Development/Runtime"
 
-##############################################################################
-## No user serviceable parts below.
-##############################################################################
+# Path to 32-bit python installation
 export PATH="/opt/pym32/bin:${PATH}"
+#############################################################################################
+
+
+
+#############################################################################################
+## No user serviceable parts below.
+#############################################################################################
 export PATH_TO_TESTS=`dirname $_`
 export PYTHONPATH=$PATH_TO_TESTS/test-scripts:$PYTHONPATH
 
 run_test() {
-    pybot  --outputdir $PATH_TO_TESTS/test-results/$1 $PATH_TO_TESTS/test-cases/$1.rest
+	pybot  --outputdir $PATH_TO_TESTS/test-results/$1 $PATH_TO_TESTS/test-cases/$1.rest
+}
+
+contains() {
+	local flag=false
+	TESTS=`ls -1 test-cases/ | grep ".rest" | awk -F '.' '{print $1}'`
+	for test in $TESTS; do
+		if [ "$1" = "$test" ]; then
+			flag=true
+		fi
+	done
+	echo "$flag"
 }
 
 if test -z "$1"; then
-    command="help"
+	command="help"
 else
-    command=$1
+	command=$1
 fi
 
 case "$command" in
-  list)
-        ls -1 test-cases/|grep ".rest"|awk -F '.' '{print $1}'
-        ;;
-  help)
-        echo "Usage: ./run-tests.sh command [parameter]"
-        echo "       ./run-tests.sh testname"
-	echo ""
-	echo "run-tests.sh is a script to launch emulator based test cases."
-	echo ""
-	echo "Commands: "
-	echo "          list           List the available test cases"
-	echo "          run <testcase> Run the particular test case"
-	echo "          run-all        Run all the test cases"
-	echo "          help           This help document"
-        ;;
-  run-all)
-	TESTS=`ls -1 test-cases/|grep ".rest"|awk -F '.' '{print $1}'`
-	for TEST in $TESTS; do
-	    run_test $TEST
-	done
-	;;
-  run)
-	run_test $2
-	;;
-  *)
-	run_test $1
-	;;
+	list)
+	    echo ""
+		ls -1 test-cases/|grep ".rest"|awk -F '.' '{print $1}'
+	    echo ""
+		;;
+	help)
+		echo ""
+		echo "Usage: ./run-tests.sh command [parameter]"
+		echo "       ./run-tests.sh testname"
+		echo ""
+		echo "run-tests.sh is a script to launch emulator based test cases."
+		echo ""
+		echo "Commands: "
+		echo "          list                  List the available test cases"
+		echo "          run <test-suite name> Run the particular test case"
+		echo "          run-all               Run all the test cases"
+		echo "          help                  This help document"
+		echo ""
+		;;
+	run-all)
+		echo ""
+		TESTS=`ls -1 test-cases/|grep ".rest"|awk -F '.' '{print $1}'`
+		for TEST in $TESTS; do
+	    	run_test $TEST
+		done
+		echo ""
+		;;
+	run)
+		echo ""
+		if test -z "$2"; then
+			echo "Please provide name of the test suite to run e.g."
+			echo "./run-tests.sh run <test-suite name>"
+		else 
+			TESTS=`ls -1 test-cases/ | grep ".rest" | awk -F '.' '{print $1}'`
+			testAvailable=false
+			testAvailable=`contains $2` 
+			if test $testAvailable = "true"; then
+				run_test $2
+			elif test $testAvailable = "false"; then
+				echo "Test name is wrong. Available tests are:"
+				echo ""
+				ls -1 test-cases/|grep ".rest"|awk -F '.' '{print $1}'
+	    	else
+	    		echo "Shell script unknown error"
+	    	fi
+		fi		
+		echo ""
+		;;
+	*)
+		echo ""
+		echo "Wrong command. Please use the following commands;"
+		echo "Commands: "
+		echo "          list                  List the available test cases"
+		echo "          run <test-suite name> Run the particular test case"
+		echo "          run-all               Run all the test cases"
+		echo "          help                  This help document"
+		echo ""
+		;;
+    #run_test $1
+	#;;
 esac
+#############################################################################################
--- test-scripts/DigitalWrite.py
+++ test-scripts/DigitalWrite.py
@@ -4,6 +4,7 @@ TEST CASES START
 """
 
 from Utilities import *
+from subprocess import call
 
 #Load the emulator shared library. Call the c-functions directly from 
 #the python script using ctypes modules.
@@ -16,6 +17,24 @@ def set_pin_mode(port, pin):
 
 def write_high(port, pin):
     emulator.digitalWrite(GPIO[port][pin], PinValue['HIGH']);
+
+    ### 
+    #Calling the sigrok command line interpretter to log output
+    #file in current directory
+    ###
+    call(
+        'sigrok-cli '+
+        '--driver saleae-logic16 '+ 
+        '--samples 1 '+ 
+        '--channels 0 '+
+        '--output-format bits '+
+        '--output-file sigrok-output ',
+        shell=True
+    )
+
+    ###
+    #Calling shell script for manipulating the log file
+    ###
 
 def write_low(port, pin):
     emulator.digitalWrite(GPIO[port][pin], PinValue['LOW']);
