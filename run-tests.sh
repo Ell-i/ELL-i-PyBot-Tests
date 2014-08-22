66cc341342f255c77d7f402d0bdd7f1d1fbdf987
Asif Sardar <engr.asif.sardar@gmail.com>
2014-08-22 18:12:23 +0300

    Modifying the run-tests.sh according to new folder structure and given command-line arguments

--- run-tests.sh
+++ run-tests.sh
@@ -6,12 +6,13 @@
 ## Setting path variables by users.
 #############################################################################################
 # Path to ELL-i Runtime
-export ELLIRUNTIME=${3}
+export ELLIRUNTIME=${4}
 #"/home/asif/Ell-i-Working-Directory/Ell-i-Software-Development/Runtime"
 
+PYTHON32=${5}
 # Path to 32-bit python installation
 #export PATH="/opt/pym32/bin:${PATH}"
-export PATH=${4}:${PATH}
+export PATH=${PYTHON32}:${PATH}
 
 #############################################################################################
 
@@ -21,10 +22,14 @@ export PATH=${4}:${PATH}
 ## No user serviceable parts below.
 #############################################################################################
 export PATH_TO_TESTS=`dirname $_`
-export PYTHONPATH=$PATH_TO_TESTS/test-scripts:$PYTHONPATH
+#export PYTHONPATH=$PATH_TO_TESTS/test-scripts:$PYTHONPATH
 
+PLATFORM=${1}
+echo "${PLATFORM}"
 run_test() {
-	pybot  --outputdir $PATH_TO_TESTS/test-results/$1 $PATH_TO_TESTS/test-cases/$1.rest
+	pybot  --pythonpath test-scripts/${PLATFORM} \
+	--outputdir $PATH_TO_TESTS/test-results/${PLATFORM}/$1 \
+	$PATH_TO_TESTS/test-cases/$1.rest
 }
 
 contains() {
@@ -38,10 +43,10 @@ contains() {
 	echo "$flag"
 }
 
-if test -z "$1"; then
+if test -z "$2"; then
 	command="help"
 else
-	command=$1
+	command=$2
 fi
 
 case "$command" in
@@ -74,15 +79,15 @@ case "$command" in
 		;;
 	run)
 		echo ""
-		if test -z "$2"; then
+		if test -z "$3"; then
 			echo "Please provide name of the test suite to run e.g."
 			echo "./run-tests.sh run <test-suite name>"
 		else 
 			TESTS=`ls -1 test-cases/ | grep ".rest" | awk -F '.' '{print $1}'`
 			testAvailable=false
-			testAvailable=`contains $2` 
+			testAvailable=`contains $3` 
 			if test $testAvailable = "true"; then
-				run_test $2
+				run_test $3
 			elif test $testAvailable = "false"; then
 				echo "Test name is wrong. Available tests are:"
 				echo ""
@@ -103,7 +108,5 @@ case "$command" in
 		echo "          help                  This help document"
 		echo ""
 		;;
-    #run_test $1
-	#;;
 esac
 #############################################################################################
